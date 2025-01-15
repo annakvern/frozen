9c6e4c7a2152d2750c2765306234673c0011f6c7
Kristin-98 <kristi98_s@hotmail.se>
2025-01-15 09:28:06 +0100

    playerInstruction test

---
+++ src/playerInstruction.ts
@@ -0,0 +1,11 @@
+class playerInstruction {
+    private drawTitle(): void;
+    private drawText(): void;
+    private drawPlayers(): void;
+    private drawPlayerKeys(): void;
+    private drawSound(): void;
+
+    constructor () {
+        
+    }
+}
