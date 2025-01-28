299f21a955a8f4dcf28877936328c299cd5e863e
Carl-Thilandergit <carl.thilander@medieinstitutet.se>
2025-01-28 13:34:50 +0100

    Teleports now find eachother. Parprogrammering med hela gruppen & David

--- src/gameBoard.ts
+++ src/gameBoard.ts
@@ -126,22 +126,14 @@ class GameBoard implements Scene {
               o2.position.x > 200 &&
               o1.dropTimer < -100
             ) {
-              o1.position.y = o2.position.y + 15;
-              o1.position.x = o2.position.x + 15;
-              o1.speed.y = 0;
-              o1.dropTimer = 500;
-              o1.setPosition("left");
-            }
-            if (
-              o2 instanceof Teleport &&
-              o2.position.x < 200 &&
-              o1.dropTimer < -100
-            ) {
-              o1.position.y = o2.position.y + 15;
-              o1.position.x = o2.position.x + 15;
-              o1.speed.y = 0;
-              o1.dropTimer = 500;
-              o1.setPosition("right");
+              for (const other of this.gameObjects) {
+                if (other instanceof Teleport && other !== o2) {
+                  o1.position.y = other.position.y + 15;
+                  o1.position.x = other.position.x + 15;
+                  o1.speed.y = 0;
+                  o1.dropTimer = 500;
+                }
+              }
             }
           }
         }
